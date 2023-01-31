c09155f8290667d7a3a7002c8344f0e6b7e00039
Allysson Oliveira <allysson.oliveira@capgemini.com>
2023-01-31 03:19:13 -0300

    adicionado novas funcionalidades

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { PokeapiService } from 'src/app/service/pokeapi.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit  {
+
+
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon'
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species'
+
+  public pokemon: any;
+
+  constructor(private activedRoute: ActivatedRoute, private pokeApiService: PokeapiService ){
+
+  }
+
+  ngOnInit(): void {
+    this.getPokemon;
+  }
+
+  get getPokemon(){
+    const id = this.activedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(
+      res=>{
+         console.log(res);
+
+      }
+    )
+
+
+  }
 
 }
